a00f01cd76ba31c6d437393b7337707f93228dff
ashishb888 <ashish.bhosle008@gmail.com>
2021-02-24 18:36:24 +0530

    sb-jenkins-gradle: Initial commit

---
+++ src/main/java/poc/jenkins/SbJenkinsGradleApplication.java
@@ -0,0 +1,13 @@
+package poc.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJenkinsGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbJenkinsGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/poc/jenkins/SbJenkinsGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package poc.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbJenkinsGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
